5b38ebc4ddac439f6c004a90144b6f773d072bda
qasimnauman-emumba <muhammadqasim.nauman@emumba.com>
2026-09-09 20:45:53 +0500

    Added Health Check

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
+app.get('/health', function(request, response) {
+  const status = {
+    "status":"healthy"
+  }
+
+  response.status(200).json(status)
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
